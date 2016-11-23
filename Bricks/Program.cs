e7f01f44ca9f6d27dfcf70becfef056f8fe58f94
Atanas Nikolov <atanas.nikolov74@gmail.com>
2016-11-23 21:57:25 +0200

    Test

---
+++ Bricks/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bricks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bricks = int.Parse(Console.ReadLine());
+            var worker = int.Parse(Console.ReadLine());
+            var volumeOfCar = int.Parse(Console.ReadLine());
+
+            var courses =Math.Ceiling((decimal)bricks /( worker * volumeOfCar));
+            Console.WriteLine(courses);
+        }
+    }
+}
---
+++ PointonSegment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PointonSegment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var first = int.Parse(Console.ReadLine());
+            var second = int.Parse(Console.ReadLine());
+            var point = int.Parse(Console.ReadLine());
+
+            
+
+            if (point>=Math.Min(first, second)&&point<=Math.Max(first, second))
+            {
+                Console.WriteLine("in");
+                Console.WriteLine("{0}", Math.Min(Math.Abs(point - second), Math.Abs(point - first)));
+            }
+            else
+            {
+                Console.WriteLine("out");
+                Console.WriteLine("{0}", Math.Min(Math.Abs(point - second), Math.Abs(point - first)));
+            }
+        }
+    }
+}
